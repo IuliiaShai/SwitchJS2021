c3386eea201c681d995d24f970c2a0d082707b8f
IuliiaShai <73893359+IuliiaShai@users.noreply.github.com>
2021-04-28 19:18:11 +0300

    add chandes to HW#6

--- HW6/HandlingErrors_Debuging_Result.js
+++ HW6/HandlingErrors_Debuging_Result.js
@@ -1,46 +1,29 @@
-//1. Write your own Promise class with syncThen method.
-
-class MyPromise {
-  constructor(executor) {
-    executor(this.resolve.bind(this));
-  }
-
-  queue = [];
-
-  resolve() {
-    queueMicrotask(() => {
-      this.queue.map((fn) => {
-        fn();
-      });
-    });
-  }
-  synchThen(fn) {
-    fn();
-    return this;
-  }
-
-  then(fn) {
-    queue.push(fn); //remeved "this" from the line
-    return this;
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "AgeIsNotValid";
   }
 }
+
+const random = () => {
+  const randomNum = Math.floor(Math.random() * 100);
+  if (randomNum < 18) {
+    throw new ValidationError("Oops! Too young =(");
+  }
+  if (randomNum >= 40 && randomNum <= 59) {
+    throw new ValidationError("it's not for you, buddy!");
+  }
+  if (randomNum >= 60) {
+    throw new ValidationError("Oops! Too old =(");
+  } else {
+    console.log("Welcome =)");
+  }
+  return randomNum;
+};
 
 try {
-  let promise = new MyPromise((resolve) => {
-    console.log(1);
-    resolve();
-  })
-    .synchThen(() => {
-      console.log(2);
-    })
-    .then(() => {
-      console.log(3);
-    });
-  console.log(4);
+  random();
 } catch (err) {
-  console.log(`${err.name}: ${err.message}`);
-} finally {
-  console.log(5);
+  console.log(err.name);
+  console.log(err.message);
 }
-
-// expected result: 1, 2, 4, 3
